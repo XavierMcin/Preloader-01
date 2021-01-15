5a34c71497a2c786ffc71b3b595d570ae475a6d7
Javier <xavier.mcin@gmail.com>
2021-01-15 15:42:43 -0500

    finished need to add fade to main page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-let button = document.querySelector('.clicker'),
+let fullBattery = document.querySelector('.battery'),
+    endBattery = document.querySelectorAll('.end'),
     singleBatt = document.querySelectorAll('span');
-    nButton = Array.from(singleBatt);
+    sBatt = Array.from(singleBatt);
+    eBatt = Array.from(endBattery);
 
-button.addEventListener('click', () => {
-    nButton.forEach((curr) => {
-        curr.classList.add('full');
-    });
+window.addEventListener('load', () => {
+
+    setTimeout(() => {
+
+        fullBattery.classList.add('done');
+
+        sBatt.forEach((curr,index) => {
+
+            if (index === sBatt.length - 1) {
+                curr.classList.add('stopper');
+            } else {
+                curr.classList.add('full');
+            }
+    
+        });
+
+        eBatt.forEach((curr) => {
+            curr.classList.add('done');
+        });
+    },8500);
+
 });
 
